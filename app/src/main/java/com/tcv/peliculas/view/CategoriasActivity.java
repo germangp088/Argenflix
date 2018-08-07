945227a5264c077fa5fcad57f5493431e97d41a8
mcRodro <31522766+mcRodro@users.noreply.github.com>
2018-08-07 09:26:48 -0700

    Fix aproximado del parallax

--- app/src/main/java/com/tcv/peliculas/view/CategoriasActivity.java
+++ app/src/main/java/com/tcv/peliculas/view/CategoriasActivity.java
@@ -116,46 +116,11 @@ public class CategoriasActivity extends AppCompatActivity
     public void onScrollChanged(int deltaX, int deltaY) {
 
         //Easy version.
-        /*int scrollY = mScrollView.getScrollY();
+        int scrollY = mScrollView.getScrollY();
         // Add parallax effect
-        imgContainer.setTranslationY(scrollY * 0.5f);*/
+        imgContainer.setTranslationY(scrollY * 0.75f);
 
-        // Get scroll view screen bound
-        Rect scrollBounds = new Rect();
-        mScrollView.getHitRect(scrollBounds);
 
-        // Check if image container is visible in the screen
-        // so to apply the translation only when the container is visible to the user
-        if (imgContainer.getLocalVisibleRect(scrollBounds)) {
-            Display display = getWindowManager().getDefaultDisplay();
-            DisplayMetrics outMetrics = new DisplayMetrics();
-            display.getMetrics(outMetrics);
-            // Get screen density
-            float density = getResources().getDisplayMetrics().density;
-
-            // Get screen height in pixels
-            float dpHeight = outMetrics.heightPixels / density;
-            int screen_height_pixels = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpHeight, getResources().getDisplayMetrics());
-            int half_screen_height = screen_height_pixels / 2;
-
-            // Get image container height in pixels
-            int container_height_pixels = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 200, getResources().getDisplayMetrics());
-
-            // Get the location that consider a vertical center for the image (where the translation should be zero)
-            int center = half_screen_height - (container_height_pixels / 2);
-
-            // get the location (x,y) of the image container in pixels
-            int[] loc_screen = {0, 0};
-            imgContainer.getLocationOnScreen(loc_screen);
-
-            // trying to transform the current image container location into percentage
-            // so when the image container is exaclty in the middle of the screen percentage should be zero
-            // and as the image container getting closer to the edges of the screen should increase to 100%
-            int final_loc = ((loc_screen[1] - center) * 100) / half_screen_height;
-
-            // translate the inner image taking consideration also the density of the screen
-            mImageView.setTranslationY(-final_loc * 0.4f * density);
-        }
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
